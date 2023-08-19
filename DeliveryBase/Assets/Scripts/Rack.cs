805b6af0395b273d403636340efa83946d73c146
VRLab_Com <ericjangdev@gmail.com>
2023-08-19 15:43:38 +0900

    Modify arr

---
+++ DeliveryBase/Assets/Scripts/Rack.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rack : MonoBehaviour
+{
+    private List<GameObject> trays = new List<GameObject>();
+    
+    
+    
+}
